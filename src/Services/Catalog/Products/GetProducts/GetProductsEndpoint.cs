012b21210fae715f65b362a1a582f28f245758ca
kirill.yakubov <kirillyakubov101@gmail.com>
2025-06-29 16:05:41 -0400

    Adding Post and GET for product

---
+++ src/Services/Catalog/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace CatalogAPI.Products.GetProducts;
+
+//public record GetProductsRequest();
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products",
+            async (ISender sender) =>
+            {
+                var request = new GetProductsQuery();
+                var result = await sender.Send(request);
+                var response = result.Adapt<GetProductsResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+    }
+}
---
+++ src/Services/Catalog/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace CatalogAPI.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery Query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", Query);
+        var products =  await session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(products);
+    }
+}
